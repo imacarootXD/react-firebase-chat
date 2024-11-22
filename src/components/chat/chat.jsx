de38f639e01db0936f1223eed546d60c97f870c8
imacaroot <imacarootation@gmail.com>
2024-11-22 19:28:28 +0000

    49:33

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
+    const [open,setOpen] = useState(false);
+    const [text,settext] = useState("");
+    const handleemoji = e => {settext((prev) => prev + e.emoji); setOpen(false)};
     return (
       <div className='chat'>
          <div className="top">
@@ -17,8 +22,49 @@ const Chat = () => {
                <img src="./info.png" alt="" />
             </div>
          </div>
-         <div className="center"></div>
-         <div className="bottom"></div>
+         <div className="center">
+            <div className="message">
+               <img src="./avatar.png" alt="" />
+               <div className="texts">
+                  <p> this is the message this is the messagethis is the messagethis is the messagethis is the messagethis is the messagethis is the messagethis is the message</p>
+                  <span>x time ago</span>
+               </div>
+            </div>
+            <div className="message own">
+               <div className="texts">
+                  <p> this is the message this is the messagethis is the messagethis is the messagethis is the messagethis is the messagethis is the messagethis is the message</p>
+                  <span>x time ago</span>
+               </div>
+            </div>
+            <div className="message">
+               <img src="./avatar.png" alt="" />
+               <div className="texts">
+                  <p> this is the message this is the messagethis is the messagethis is the messagethis is the messagethis is the messagethis is the messagethis is the message</p>
+                  <span>x time ago</span>
+               </div>
+            </div>
+            <div className="message own">
+               <div className="texts">
+                  <p> this is the message this is the messagethis is the messagethis is the messagethis is the messagethis is the messagethis is the messagethis is the message</p>
+                  <span>x time ago</span>
+               </div>
+            </div>
+         </div>
+         <div className="bottom">
+             <div className="icons">
+               <img src="./img.png" alt="" />
+               <img src="./camera.png" alt="" />
+               <img src="./mic.png" alt="" />
+             </div>
+             <input type="text" placeholder="Type a message..." value={text} onChange={e=>settext(e.target.value)}/>
+             <div className="emoji">
+                <img src="./emoji.png" alt="" onClick={() =>setOpen((prev => !prev))} />
+                <div className="picker">
+                  <EmojiPicker open={open} onEmojiClick={handleemoji} />
+                </div>
+             </div>
+             <button className="sendButton">send</button>
+         </div>
       </div>
    )
 }
